9d6103822906d34c99dfeff49cf4ec76c296281c
Manfred Gawlas <manfredgawlas12@gmail.com>
2025-03-20 23:52:54 +0000

    fk does not check stuff from Large directory, prevents bloat from home

--- Scripts/fk.sh
+++ Scripts/fk.sh
@@ -1,6 +1,6 @@
 fk() {
     local selected
-    selected=$(find . -type d -not -path '*/\.*' | fzf)
+    selected=$(find . -type d -not -path '*/\.*' -not -path '*/Large*' | fzf)
     if [[ -n $selected ]]; then
         cd "$selected"
     fi
