ec6af162d350877ceeba179364ea9c7eea404e2b
Manfred Gawlas <manfredgawlas12@gmail.com>
2026-03-15 17:35:15 +0100

    fk updated with fd-find

--- Scripts/fk.sh
+++ Scripts/fk.sh
@@ -1,19 +1,9 @@
 fk() {
+    # Updated to use fdfind - ripgrep find alternative for faster, now significantly better
     local selected
-    selected=$(find /home/prlpnrpph512/Documents -type d \
-        -not -path '*/Large*' \
-        -not -path '*/build*' \
-        -not -path '*/.git*' \
-        -not -path '*/.venv*' \
-        -not -path '*/.github*' \
-        -not -path '*/.__pycache__*' \
-        -not -path '*/.cache*' \
-        -not -path '*/.svn*' \
-        -printf '%P\n' \
-        | awk '{ print length($0), $0 }' \
-        | sort -n \
-        | cut -d" " -f2- \
-        | fzf)
+
+    selected=$(cd /home/prlpnrpph512/Documents && fdfind --type d | fzf --tiebreak=length)
+
     if [[ -n $selected ]]; then
         cd "/home/prlpnrpph512/Documents/$selected"
     fi
